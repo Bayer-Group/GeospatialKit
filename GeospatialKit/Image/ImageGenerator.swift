236166ce7deca9181ea85ca1102fdd0599f5f66e
Vernon Schierding <schlingding@gmail.com>
2018-07-24 07:54:37 -0500

    Logic added to assist in resizing map image with MKSnapshotter. Not yet accessible from outside the Library.

--- GeospatialKit/Image/ImageGenerator.swift
+++ GeospatialKit/Image/ImageGenerator.swift
@@ -3,16 +3,24 @@ public protocol ImageGeneratorProtocol {
     func snapshot(for geoJsonObject: GeoJsonObject, with imageRenderModel: ImageRenderModel, debug: Bool, completion: @escaping (UIImage?) -> Void)
 }
 
-internal struct ImageGenerator: ImageGeneratorProtocol {
+internal struct SnapshotSettings {
+    let snapshot: MKMapSnapshot
+    let scale: Double
+}
+
+internal class ImageGenerator: ImageGeneratorProtocol {
     func image(for geoJsonObject: GeoJsonObject, with imageRenderModel: ImageRenderModel, debug: Bool) -> UIImage? {
-        return image(for: geoJsonObject, with: imageRenderModel, snapshot: nil, debug: debug)
+        return image(for: geoJsonObject, with: imageRenderModel, snapshotSettings: nil, debug: debug)
     }
     
     func snapshot(for geoJsonObject: GeoJsonObject, with imageRenderModel: ImageRenderModel, debug: Bool, completion: @escaping (UIImage?) -> Void) {
         guard let region = geoJsonObject.objectBoundingBox?.mappingBoundingBox(insetPercent: imageRenderModel.inset).region else { completion(nil); return }
         
-        let width = imageRenderModel.width * Double(UIScreen.main.scale)
-        let height = imageRenderModel.height * Double(UIScreen.main.scale)
+        // SOMEDAY: Decide snapshot scale - the number will vary based on if latitude or longitude is chosen as the limiting factor based on view width and height.
+        //let shouldScale = max(region.span.longitudeDelta, region.span.latitudeDelta) < 0.002880360609112
+        let snapshotScale = 1.0 //shouldScale ? max(region.span.longitudeDelta, region.span.latitudeDelta) * 805000 : 1
+        let width = imageRenderModel.width * Double(UIScreen.main.scale) / snapshotScale
+        let height = imageRenderModel.height * Double(UIScreen.main.scale) / snapshotScale
         
         let mapSnapshotOptions = MKMapSnapshotOptions()
         mapSnapshotOptions.region = region
@@ -27,7 +35,9 @@ internal struct ImageGenerator: ImageGeneratorProtocol {
             
             guard let snapshot = snapshot else { return }
             
-            let finalImage = self.image(for: geoJsonObject, with: imageRenderModel, snapshot: snapshot, debug: debug)
+            let snapshotSettings = SnapshotSettings(snapshot: snapshot, scale: snapshotScale)
+            
+            let finalImage = self.image(for: geoJsonObject, with: imageRenderModel, snapshotSettings: snapshotSettings, debug: debug)
             
             completion(finalImage)
         }
@@ -35,7 +45,7 @@ internal struct ImageGenerator: ImageGeneratorProtocol {
 }
 
 extension ImageGenerator {
-    private func image(for geoJsonObject: GeoJsonObject, with imageRenderModel: ImageRenderModel, snapshot: MKMapSnapshot?, debug: Bool) -> UIImage? {
+    private func image(for geoJsonObject: GeoJsonObject, with imageRenderModel: ImageRenderModel, snapshotSettings: SnapshotSettings?, debug: Bool) -> UIImage? {
         let width = imageRenderModel.width * Double(UIScreen.main.scale)
         let height = imageRenderModel.height * Double(UIScreen.main.scale)
         
@@ -47,8 +57,8 @@ extension ImageGenerator {
         
         let renderer = UIGraphicsImageRenderer(size: desiredImageRect.size, format: rendererFormat)
         return renderer.image { context in
-            if let snapshot = snapshot {
-                snapshot.image.draw(in: desiredImageRect)
+            if let snapshotSettings = snapshotSettings {
+                snapshotSettings.snapshot.image.draw(in: desiredImageRect)
             } else {
                 imageRenderModel.backgroundColor.setFill()
                 context.fill(desiredImageRect)
@@ -62,36 +72,36 @@ extension ImageGenerator {
             let pointProjector = PointProjector(boundingBox: insetBoundingBox, width: width, height: height)
             
             geometries.forEach {
-                drawGeometry(imageRenderModel: imageRenderModel, pointProjector: pointProjector, geometry: $0, context: context.cgContext, snapshot: snapshot, debug: debug)
+                drawGeometry(imageRenderModel: imageRenderModel, pointProjector: pointProjector, geometry: $0, context: context.cgContext, snapshotSettings: snapshotSettings, debug: debug)
             }
         }
     }
     
-    private func drawGeometry(imageRenderModel: ImageRenderModel, pointProjector: PointProjector, geometry: GeoJsonGeometry, context: CGContext, snapshot: MKMapSnapshot?, debug: Bool) {
+    private func drawGeometry(imageRenderModel: ImageRenderModel, pointProjector: PointProjector, geometry: GeoJsonGeometry, context: CGContext, snapshotSettings: SnapshotSettings?, debug: Bool) {
         switch geometry {
         case let point as GeoJsonPoint:
-            drawPin(imageRenderModel: imageRenderModel, pointProjector: pointProjector, context: context, snapshot: snapshot, point: point)
+            drawPin(imageRenderModel: imageRenderModel, pointProjector: pointProjector, context: context, snapshotSettings: snapshotSettings, point: point)
         case let multiPoint as GeoJsonMultiPoint:
-            multiPoint.points.forEach { drawPin(imageRenderModel: imageRenderModel, pointProjector: pointProjector, context: context, snapshot: snapshot, point: $0) }
+            multiPoint.points.forEach { drawPin(imageRenderModel: imageRenderModel, pointProjector: pointProjector, context: context, snapshotSettings: snapshotSettings, point: $0) }
         case let lineString as GeoJsonLineString:
-            drawLine(imageRenderModel: imageRenderModel, context: context, snapshot: snapshot, pointProjector: pointProjector, line: lineString, debug: debug)
+            drawLine(imageRenderModel: imageRenderModel, context: context, snapshotSettings: snapshotSettings, pointProjector: pointProjector, line: lineString, debug: debug)
         case let multiLineString as GeoJsonMultiLineString:
-            multiLineString.lineStrings.forEach { drawLine(imageRenderModel: imageRenderModel, context: context, snapshot: snapshot, pointProjector: pointProjector, line: $0, debug: debug) }
+            multiLineString.lineStrings.forEach { drawLine(imageRenderModel: imageRenderModel, context: context, snapshotSettings: snapshotSettings, pointProjector: pointProjector, line: $0, debug: debug) }
         case let polygon as GeoJsonPolygon:
-            drawPolygon(imageRenderModel: imageRenderModel, context: context, snapshot: snapshot, pointProjector: pointProjector, polygon: polygon, debug: debug)
+            drawPolygon(imageRenderModel: imageRenderModel, context: context, snapshotSettings: snapshotSettings, pointProjector: pointProjector, polygon: polygon, debug: debug)
             
-            if debug { drawPin(imageRenderModel: imageRenderModel, pointProjector: pointProjector, context: context, snapshot: snapshot, point: polygon.centroid) }
+            if debug { drawPin(imageRenderModel: imageRenderModel, pointProjector: pointProjector, context: context, snapshotSettings: snapshotSettings, point: polygon.centroid) }
         case let multiPolygon as GeoJsonMultiPolygon:
-            multiPolygon.polygons.forEach { drawPolygon(imageRenderModel: imageRenderModel, context: context, snapshot: snapshot, pointProjector: pointProjector, polygon: $0, debug: debug) }
+            multiPolygon.polygons.forEach { drawPolygon(imageRenderModel: imageRenderModel, context: context, snapshotSettings: snapshotSettings, pointProjector: pointProjector, polygon: $0, debug: debug) }
         case let geometryCollection as GeoJsonGeometryCollection:
             geometryCollection.objectGeometries?.forEach {
-                drawGeometry(imageRenderModel: imageRenderModel, pointProjector: pointProjector, geometry: $0, context: context, snapshot: snapshot, debug: debug)
+                drawGeometry(imageRenderModel: imageRenderModel, pointProjector: pointProjector, geometry: $0, context: context, snapshotSettings: snapshotSettings, debug: debug)
             }
         default: return
         }
     }
     
-    private func drawPin(imageRenderModel: ImageRenderModel, pointProjector: PointProjector, context: CGContext, snapshot: MKMapSnapshot?, point: GeodesicPoint) {
+    private func drawPin(imageRenderModel: ImageRenderModel, pointProjector: PointProjector, context: CGContext, snapshotSettings: SnapshotSettings?, point: GeodesicPoint) {
         let pinView = MKPinAnnotationView(annotation: nil, reuseIdentifier: nil)
         if let pinTintColor = imageRenderModel.pinTintColor { pinView.pinTintColor = pinTintColor }
         let pinImage = pinView.image!
@@ -100,8 +110,10 @@ extension ImageGenerator {
         let height = pinImage.size.height * UIScreen.main.scale
         
         var cgPoint: CGPoint
-        if let snapshot = snapshot {
-            cgPoint = snapshot.point(for: point.locationCoordinate)
+        if let snapshotSettings = snapshotSettings {
+            let scale = CGFloat(snapshotSettings.scale)
+            let cgPointTemp = snapshotSettings.snapshot.point(for: point.locationCoordinate)
+            cgPoint = CGPoint(x: cgPointTemp.x * scale, y: cgPointTemp.y * scale)
         } else {
             cgPoint = pointProjector.asPoints([point]).first!
         }
@@ -114,12 +126,16 @@ extension ImageGenerator {
         pinImage.draw(in: CGRect(origin: cgPoint, size: CGSize(width: width, height: height)))
     }
     
-    private func drawLine(imageRenderModel: ImageRenderModel, context: CGContext, snapshot: MKMapSnapshot?, pointProjector: PointProjector, line: GeoJsonLineString, debug: Bool) {
+    private func drawLine(imageRenderModel: ImageRenderModel, context: CGContext, snapshotSettings: SnapshotSettings?, pointProjector: PointProjector, line: GeoJsonLineString, debug: Bool) {
         let points = line.points
         
         let cgPoints: [CGPoint]
-        if let snapshot = snapshot {
-            cgPoints = points.map { snapshot.point(for: $0.locationCoordinate) }
+        if let snapshotSettings = snapshotSettings {
+            cgPoints = points.map {
+                let scale = CGFloat(snapshotSettings.scale)
+                let cgPointTemp = snapshotSettings.snapshot.point(for: $0.locationCoordinate)
+                return CGPoint(x: cgPointTemp.x * scale, y: cgPointTemp.y * scale)
+            }
         } else {
             cgPoints = pointProjector.asPoints(points)
         }
@@ -133,10 +149,10 @@ extension ImageGenerator {
         
         context.strokePath()
         
-        if debug { points.forEach { drawPin(imageRenderModel: imageRenderModel, pointProjector: pointProjector, context: context, snapshot: snapshot, point: $0) } }
+        if debug { points.forEach { drawPin(imageRenderModel: imageRenderModel, pointProjector: pointProjector, context: context, snapshotSettings: snapshotSettings, point: $0) } }
     }
     
-    private func drawPolygon(imageRenderModel: ImageRenderModel, context: CGContext, snapshot: MKMapSnapshot?, pointProjector: PointProjector, polygon: GeoJsonPolygon, debug: Bool) {
+    private func drawPolygon(imageRenderModel: ImageRenderModel, context: CGContext, snapshotSettings: SnapshotSettings?, pointProjector: PointProjector, polygon: GeoJsonPolygon, debug: Bool) {
         context.setLineWidth(CGFloat(imageRenderModel.lineWidth))
         context.setStrokeColor(imageRenderModel.shapeLineColor.cgColor)
         context.setFillColor(imageRenderModel.shapeFillColor.cgColor)
@@ -145,8 +161,12 @@ extension ImageGenerator {
         
         polygon.linearRings.reversed().forEach { linearRing in
             let points: [CGPoint]
-            if let snapshot = snapshot {
-                points = linearRing.points.map { snapshot.point(for: $0.locationCoordinate) }
+            if let snapshotSettings = snapshotSettings {
+                points = linearRing.points.map {
+                    let scale = CGFloat(snapshotSettings.scale)
+                    let cgPointTemp = snapshotSettings.snapshot.point(for: $0.locationCoordinate)
+                    return CGPoint(x: cgPointTemp.x * scale, y: cgPointTemp.y * scale)
+                }
             } else {
                 points = pointProjector.asPoints(linearRing.points)
             }
@@ -160,9 +180,9 @@ extension ImageGenerator {
         context.drawPath(using: .eoFillStroke)
         
         if debug {
-            drawPin(imageRenderModel: imageRenderModel, pointProjector: pointProjector, context: context, snapshot: snapshot, point: polygon.centroid)
+            drawPin(imageRenderModel: imageRenderModel, pointProjector: pointProjector, context: context, snapshotSettings: snapshotSettings, point: polygon.centroid)
             
-            polygon.linearRings.forEach { $0.points.forEach { drawPin(imageRenderModel: imageRenderModel, pointProjector: pointProjector, context: context, snapshot: snapshot, point: $0) } }
+            polygon.linearRings.forEach { $0.points.forEach { drawPin(imageRenderModel: imageRenderModel, pointProjector: pointProjector, context: context, snapshotSettings: snapshotSettings, point: $0) } }
         }
     }
 }
