2d1999a0d597204217e9e6340b5818b15e082f23
Vernon Schierding <vernon.schierding@bayer.com>
2020-02-17 09:17:41 -0600

    Version and logging name updates.

--- GeospatialKit/Timber/Timber.swift
+++ GeospatialKit/Timber/Timber.swift
@@ -1,5 +1,5 @@
 import TimberSwift
 
-private let timber: TimberProtocol = Timber(source: Source(title: "Geospatial Swift", version: Bundle.sourceVersion, emoji: "🗺️"))
+private let timber: TimberProtocol = Timber(source: Source(title: "Geospatial Kit", version: Bundle.sourceVersion, emoji: "🗺️"))
 
 internal let Log = timber.log
