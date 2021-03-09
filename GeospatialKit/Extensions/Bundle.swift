ca2d00bc59d0ac5891b426e9afe13e15c962e9e3
Vernon Schierding <vernon.schierding@bayer.com>
2021-03-08 20:41:27 -0600

    SPM fix

--- GeospatialKit/Extensions/Bundle.swift
+++ GeospatialKit/Extensions/Bundle.swift
@@ -3,7 +3,13 @@ import Foundation
 private class BundleClass {}
 
 internal extension Bundle {
-    static var source: Bundle { return Bundle(for: BundleClass.self) }
+    static var source: Bundle {
+        #if SWIFT_PACKAGE
+        return Bundle.module
+        #else
+        return Bundle(for: BundleClass.self)
+        #endif
+    }
     
     static var sourceVersion: String { return source.infoDictionary?["CFBundleShortVersionString"] as? String ?? "" }
 }
